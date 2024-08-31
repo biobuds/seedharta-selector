9fa5e582aa7fcbc34d1f47f39f1f1423b1139e25
Luciano Ferroni <67214159+raaiider@users.noreply.github.com>
2024-08-31 19:36:07 -0400

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -157,7 +157,10 @@ tiendavalpo07@gmail.com`;
             </button>
           </div>
           <div className="flex flex-row justify-center items-center space-x-4 mb-6 flex-wrap">
-            <a href="https://wa.me/56964316344" target="_blank" rel="noopener noreferrer" className="social-button">WhatsApp</a>
+            <a href=selectedStore === 'vina' 
+                ? "https://wa.me/56964316344"
+                : "https://wa.me/56964316344"
+              } target="_blank" rel="noopener noreferrer" className="social-button">WhatsApp</a>
             <a 
               href={selectedStore === 'vina' 
                 ? "https://www.instagram.com/seedhartavinadelmar/" 
