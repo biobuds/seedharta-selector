8f4b020cb8d468ecbe081f533440b105ecbff384
Luciano Ferroni <67214159+raaiider@users.noreply.github.com>
2024-09-07 15:24:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/seedharta-selector/",
   server: {
     host: "::",
     port: "8080",
